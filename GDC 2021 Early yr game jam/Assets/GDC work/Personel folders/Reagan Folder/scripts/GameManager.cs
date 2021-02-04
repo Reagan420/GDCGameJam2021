71c9db484c84cb97e913cb8ac0d43a94d7279d09
Mebradhen <31306185+Mebradhen@users.noreply.github.com>
2021-02-04 13:59:22 +1000

    added end states

--- GDC 2021 Early yr game jam/Assets/GDC work/Personel folders/Reagan Folder/scripts/GameManager.cs
+++ GDC 2021 Early yr game jam/Assets/GDC work/Personel folders/Reagan Folder/scripts/GameManager.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
-    public double time = 0;
+    public float time = 0;
     public int numPeopleInfected = 0;
     private int totalNPCs = 20;
     public bool playerisdead = false;
 
+    // 
+
+
+    // Ui
+    public Text Infection_count;
+    public Text Infection_Timer;
 
     public static GameManager Instance { get; private set; }
     // Start is called before the first frame update
@@ -29,15 +37,48 @@ public class GameManager : MonoBehaviour
         {
             endGame(true);
         }
-        else if (numPeopleInfected == 0 && playerisdead == true)
+        else if ((numPeopleInfected == 0 && playerisdead == true) || time <= 0.0f)
         {
             endGame(false);
         }
+
+
+        time -= Time.deltaTime ;
+
+        Ui_update();
+    }
+
+    // updates the UI 
+    void Ui_update()
+    {
+        float seconds = Mathf.FloorToInt(time % 60);
+        float Mins = Mathf.FloorToInt(time / 60);
+
+        Infection_count.text = numPeopleInfected.ToString();
+        Infection_Timer.text = string.Format("{0:00}:{1:00}", Mins, seconds);
     }
 
     public void endGame(bool didwin)
     {
         //this method ends the game and did win tells us if the player has won
+
+        if(didwin == true)
+        {
+            // load player won 
+            Loadscene("3 - Won_game");
+        }
+        else
+        {
+            // player lost
+            Loadscene("2 -Game_Over");
+        }
+    }
+
+
+    // this loads the scene bois
+    private void Loadscene(string level)
+    {
+        SceneManager.LoadScene(level);
     }
 
 }
